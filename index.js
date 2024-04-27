f6f07a7b8232c101451f432105daa819a47d9b59
jeffersonbalde <jeffersonbalde8@gmail.com>
2024-04-27 14:39:47 +0800

    update api

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ app.get("/", (req,res) => {
     res.send("Hello Worlds");
 })
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// CREATE API
 app.post("/api/products", async (req,res) => {
     // console.log(req.body)
     // res.send(req.body)
@@ -17,6 +37,23 @@ app.post("/api/products", async (req,res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// UPDATE API
+app.put("/api/product/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product) {
+            return res.status(404).json({message: "Product not found"});
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
     } catch (error) {
         res.status(500).json({message: error.message})
     }
